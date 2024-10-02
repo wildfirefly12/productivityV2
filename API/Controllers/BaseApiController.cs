63c1a817719ee945a4666b9609e96ab0fe3288a9
wildfirefly12 <wildfirefly12@gmail.com>
2024-10-02 10:35:31 -0600

    created BaseApiController.cs and Result.cs

---
+++ API/Controllers/BaseApiController.cs
@@ -0,0 +1,30 @@
+﻿using Application.Core;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Productivity.Controllers;
+
+[Route("api/[controller]/[action]")]
+public class BaseApiController : ControllerBase
+{
+    private IMediator _mediator;
+
+    protected IMediator Mediator => _mediator ??= HttpContext.RequestServices.GetService<IMediator>();
+
+    protected ActionResult HandleResult<T>(Result<T> result)
+    {
+        if (result == null) return NotFound();
+        
+        if (result.IsSuccess && result.Value != null)
+        {
+            return Ok(result.Value);
+        }
+
+        if (result.IsSuccess && result.Value == null)
+        {
+            return NotFound();
+        }
+
+        return BadRequest(result.Error);
+    }
+}
